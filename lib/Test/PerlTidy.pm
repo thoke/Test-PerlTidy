9956f8fb5985db290055b5fca20abbd57255edcc
Shlomi Fish <shlomif@shlomifish.org>
2019-04-02 11:26:47 +0300

    explicit imports

--- lib/Test/PerlTidy.pm
+++ lib/Test/PerlTidy.pm
@@ -188,7 +188,7 @@ Test::PerlTidy - check that all your files are tidy.
 
     # In a file like 't/perltidy.t':
 
-    use Test::PerlTidy;
+    use Test::PerlTidy qw( run_tests );
 
     run_tests();
 
